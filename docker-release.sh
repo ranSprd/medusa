9d12a1ec5ccc2c5b0c0ccc902028b728aa8fa09d
ranSprd <an.raik@gmail.com>
2023-12-17 18:02:18 +0100

    script for docker image release

---
+++ docker-release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./mvnw package
+docker build -f src/main/docker/Dockerfile.jvm -t quarkus/medusa-jvm .
+
+SCRIPT_DIR="$(pwd)"
+echo "start container on CLI:"
+echo "docker run -i --rm -p 8085:8085 -v $SCRIPT_DIR/config:/deployments/config/  quarkus/medusa-jvm
